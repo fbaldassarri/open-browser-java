f2a6a933d6d51534075491e9ece03b64de65e1b9
Francesco Baldassarri <f.baldassarri@live.com>
2023-03-28 22:24:01 +0200

    remove wrong idention

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 // experimental test jar to verify opening of Windows OS default browser
 
 import java.awt.Desktop;
-        import java.io.*;
-        import java.net.URI;
+import java.io.*;
+import java.net.URI;
 
 class GFG {
     public static void main(String[] args)
